e4e199878ce1cb09d6d7ca2cf720b2180356648b
Jingling Zhu <zhujingling523@gmail.com>
2021-10-09 11:30:52 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RebootNotification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
